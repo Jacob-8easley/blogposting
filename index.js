846ea01fe3be44ae187c09f92563f3b827863adf
JacobBeasley <beasley.jacob@outlook.com>
2024-09-04 21:26:35 +0100

    create operating blog for single post

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
 import express from "express";
-//import ejs from "ejs";
+import {dirname} from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+const __dirname = dirname(fileURLToPath(import.meta.url))
+import ejs from "ejs";
+import { title } from "process";
 
 const app = express();
 const port = 3000;
 
 app.use(express.static("public"))
 
+app.use(bodyParser.urlencoded({extended:true}));
+//Get the homepage to submit infomation
 app.get("/",(req,res)=>{
-    res.render("partials/index.ejs");
+    res.render("views/partials/index.ejs");
 })
 
-app.get("/posted",(req,res)=>{
-    res.render("partials/submit.ejs")
-})
+
+app.post("/submit",(req,res)=>{
+    //generate and trim date to need
+    let currentDate = new Date();
+    let day = currentDate.toString();
+    let trimDate = day.slice(0,-30)
+    
+    //dictionary to store data
+    const postData = new Object();
+    
+    postData[req.body["pTitle"]] = [req.body["tBox"],trimDate];
+    const logger = Object.keys(postData);
+    console.log(logger);
+
+
+
+    //Rendering entered information onto new page
+    res.render("views/submit.ejs",{
+    title : req.body["pTitle"],
+    post : req.body["tBox"],
+    date: trimDate,
+});
+});
 
 
 
